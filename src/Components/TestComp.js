056be8d7171a3ab17cd5b2b56b3c140d49621929
manju2052 <manju.2052.2052@gmail.com>
2021-09-13 23:04:05 +0530

    first

---
+++ src/Components/TestComp.js
@@ -0,0 +1,29 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+const TestComp=()=>{
+const [data,setData]=useState({gdata:[]});
+
+
+useEffect(()=>{
+    axios.get(`http://localhost:4000/test`).then(
+        res=>{
+            setData({gdata:res.data})
+        }
+    ).catch(error=>{
+        console.log(error);
+    })
+},[])
+
+    return(
+
+        <>
+<h1>{data.gdata.data}</h1>
+
+        </>
+    )
+
+
+
+}
+export default TestComp;
